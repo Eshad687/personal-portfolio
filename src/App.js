2ec0495b96a0e0836ff480a61d009d292124b672
Eshad Dewan <eshaddewan571@gmail.com>
2021-11-29 23:27:46 +0600

    kkll

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
       <Navigation></Navigation>
       <Banner></Banner>
-      <h1 style={{ color: 'white' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, enim at. Eveniet explicabo vero doloremque autem saepe voluptatem ab asperiores dicta accusamus et quas dolor veniam, fugit, voluptatum id consectetur!</h1>
+
       <Projects></Projects>
     </div>
   );
